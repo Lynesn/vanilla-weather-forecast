02684f8e2b89cc6b5926dc5c5b7e63bff9b35731
=lyne <=lynealex@gmail.com>
2022-08-25 01:22:57 +0300

    Displayed forecast data using api.

--- homewk8.js
+++ homewk8.js
@@ -15,23 +15,32 @@ function formatDate(timestamp){
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  
   let forecastHTML = `<div class="row">`;
-  days.forEach(function(day){
-    forecastHTML = forecastHTML + 
+  forecast.forEach(function(forecastDay, index){
+    if (index < 6) {
+      forecastHTML = forecastHTML + 
                   `<div class="col-2">
-                     <div class="weather-forecast-date">${day}</div>
-                     <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="42"/>
+                     <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+                     <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42"/>
                      <div class="weather-forecast-temperatures">
-                      <span class ="weather-forecast-temperature-max"> 18° </span>
-                      <span class ="weather-forecast-temperature-min"> 12° </span>
+                      <span class ="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
+                      <span class ="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
                      </div> 
                    </div>`;
+    }
   });
 forecastHTML = forecastHTML + `</div>`;
 forecastElement.innerHTML = forecastHTML;
@@ -41,7 +50,6 @@ function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "8c43a9396234aa25ef4906a9eda3077c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
-  // console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -66,7 +74,6 @@ function displayTemperature(response){
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
   getForecast(response.data.coord);
-  // console.log(response.data);
 }
 
 function search(city) {
@@ -114,4 +121,3 @@ cel.addEventListener("click", displayCelsiusTemperature);
 
 
 search("Kampala");
-// displayForecast();
